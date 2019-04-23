181487474971e88dafa74e42c354657d226c93af
Vitalii <vitalii.luniev83@gmail.com>
2019-04-24 00:03:59 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+    var x = document.getElementById('myTopnav');
+
+    if (x.className === "myTopnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
